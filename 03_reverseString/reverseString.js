61623f8c2a79483080659ee1d9d72c83676f91e4
Phan Wilkie <phanwilkie@gmail.come>
2024-01-02 01:43:55 +1300

    Completed 3rd exercise + TDD

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //get string
+    let finalString = '';
+    // let string = 'hello';
+    
+    for (i = string.length; i >= 1; i--) {
+    //iterate by decrementing from max length
+    //grab char and append to finalString based on max length
+        letter = string[i-1];
+        finalString += letter;
+    }
+    return finalString;
+    
 };
 
 // Do not edit below this line
