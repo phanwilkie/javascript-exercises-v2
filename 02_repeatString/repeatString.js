bf264b0d31efcc8936ff6f0f19d6d75f8ac9ea27
Phan Wilkie <phanwilkie@gmail.come>
2024-01-02 00:58:00 +1300

    Completed TDD exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    let finalWord = '';
+    if (number < 0) {
+        return 'ERROR';
+    }
+    else {
+        for (i = 0; i < number; i++) {
+            finalWord += word;
+        };
+        return finalWord;
+    
+    };
 };
 
 // Do not edit below this line
