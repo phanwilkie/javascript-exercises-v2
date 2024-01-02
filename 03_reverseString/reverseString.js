f5c99853dbf78be28305519655767c2503f67288
Phan Wilkie <phanwilkie@gmail.come>
2024-01-03 11:25:56 +1300

    Completed LeapYear exercise and updated reversestring.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,6 @@
 const reverseString = function(string) {
-    //get string
     let finalString = '';
-    // let string = 'hello';
-    
     for (i = string.length; i >= 1; i--) {
-    //iterate by decrementing from max length
-    //grab char and append to finalString based on max length
         letter = string[i-1];
         finalString += letter;
     }
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //if year's last 2 digits end with 00
+    // % 400 === 0; else it's not a leap year
+    if (String(year).slice(-2) == '00') {
+        if (year % 400 === 0) {return true;} else { return false; }
+    }
+    //if year's last 2 digits does NOT end with 00
+    // % 4 === 0; else it's not a leap year
+    else { 
+        if (year % 4 === 0) { return true; } else { return false; }
+    };
+ 
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//better and leaner solution below:
+//return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
