8870d41805d94e35f59b23d41899423b7451c8cd
Phan Wilkie <phanwilkie@gmail.come>
2024-01-02 22:48:55 +1300

    Completed Exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+//checking for elements (num) in myValues that are not present in the myRemove array. 
+//It filters out those elements from myValues and returns a new array containing only 
+//the elements that satisfy this condition
 
+const removeFromArray = function(myValues, ...myRemove) {
+    return myValues.filter(num => !myRemove.includes(num));
 };
 
 // Do not edit below this line
