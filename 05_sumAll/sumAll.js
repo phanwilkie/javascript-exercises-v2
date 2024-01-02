95f59e6156f92da67ecb28d78e26e19718e18805
Phan Wilkie <phanwilkie@gmail.come>
2024-01-03 10:59:43 +1300

    Completed exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sumArray = 0;
 
+    //return error if parameters contains a negative number or non-number
+    if (start < 0 || 
+        end < 0 || 
+        (typeof start) !== 'number' || 
+        (typeof end) !== 'number') {
+        return 'ERROR';
+    };
+
+    //swap the parameters if min > max
+    if (start > end) {
+        min = end;
+        max = start;
+    } else {
+        min = start;
+        max = end;
+    };
+
+    for (let i = min; i <= max; i++) {
+        sumArray += i;
+    };
+    return sumArray;
 };
 
 // Do not edit below this line
